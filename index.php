7f59d1216715f06a041835d551d0f98adf91dddc
Dmitry Akhinyan <113758346+Alixtre@users.noreply.github.com>
2022-11-21 14:41:16 +0200

    updated function Find

--- index.php
+++ index.php
@@ -10,15 +10,14 @@ function AddtoArray(string $filename): array
     fclose($file);
     return $array;
 }
-function Find(string $login, string $password, array $array): array
+function Find(string $login, string $password, array $array): array|bool
 {
     foreach ($array as $value) {
         if (($value[1] == $login) && ($value[2] == $password)) {
             return $value;
         }
     }
-    echo ("<br>User not found");
-    return [null, null, null, null, null];
+    return false;
 }
 
 $array = AddtoArray(FILES_PATH . "Logins.txt");
